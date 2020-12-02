1bec55fcb7f9ac302a2a8681414228395cfae42c
Sasha Grinevich <alexandr.grinevich@gmail.com>
2020-12-02 13:27:30 +0200

    feat: chapter 2 // details // copy

--- src/components/Chapter2/slides/12.Details.js
+++ src/components/Chapter2/slides/12.Details.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { BrowserView, MobileView } from "react-device-detect";
 import { H4, P } from '../../../styles/typography'
-import { Column } from '../../UIKit';
+import { Column, Layer } from '../../UIKit';
 import Slide from '../Slide';
 import { WatchesSafeArea } from '../Watches';
 import { animateFadeInOut } from '../../../helpers/animation';
@@ -12,6 +12,8 @@ const CircledImage = styled.img`
   width: auto !important;
   border: 1px solid ${props => props.theme.borderColor};
   border-radius: 50%;
+  height: auto !important;
+  max-height: 100% !important;
 `;
 
 export default function Details({ index, data: d }) {
@@ -25,13 +27,31 @@ export default function Details({ index, data: d }) {
               transform: translate(0px, 10%);
             `} />
           </WatchesSafeArea>
+
+          <Layer left="0">  
+            <WatchesSafeArea justify="flex-end">
+              <Column w="50%" h="100%" align="center">
+                <P css={css`max-width: 50%;`}>
+                  {d.copy[0] ? d.copy[0].text : ''}
+                </P>
+              </Column>
+            </WatchesSafeArea>
+          </Layer>
         </Slide>
       </BrowserView>
 
       <MobileView renderWithFragment>
         <Slide index={index} animate={animateFadeInOut}>
-          <Column h="100%" w="100%" justify="center" css={css`padding: 0 35px;`}>
-          <CircledImage {...d.images[0]} />
+          <Column h="100%" w="100%" justify="flex-start" css={css`padding: 0 35px; `}>
+            <Column css={css`flex: 1; margin: 20px 0;`}>
+              <Layer css={css`text-align: center;`}>
+                <CircledImage {...d.images[0]} />
+              </Layer>
+            </Column>
+
+            <P mobile align="center">
+              {d.copy[0] ? d.copy[0].text : ''}
+            </P>
           </Column>
         </Slide>
       </MobileView>
